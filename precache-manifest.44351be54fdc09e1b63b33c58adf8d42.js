68afc1066f3a4beb05257dc3cd1043c2d5b677a0
yaromyrshmid <yaromyr.shmid@gmail.com>
2019-09-07 19:32:33 +0300

    Updates

---
+++ precache-manifest.44351be54fdc09e1b63b33c58adf8d42.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "813635c8058771f4d548c0c81e2f1627",
+    "url": "/ColorPro/index.html"
+  },
+  {
+    "revision": "3179250f3d915160d63f",
+    "url": "/ColorPro/static/css/main.509edcb4.chunk.css"
+  },
+  {
+    "revision": "98a7a830897239b80864",
+    "url": "/ColorPro/static/js/2.cdb92da8.chunk.js"
+  },
+  {
+    "revision": "3179250f3d915160d63f",
+    "url": "/ColorPro/static/js/main.acc255f0.chunk.js"
+  },
+  {
+    "revision": "612035c08e05d3e7775d",
+    "url": "/ColorPro/static/js/runtime~main.ce6a04c8.js"
+  },
+  {
+    "revision": "55dad71f8d20c8ad2536bdc777cd2a98",
+    "url": "/ColorPro/static/media/FB_icon.55dad71f.svg"
+  },
+  {
+    "revision": "ab350240affcc4281102e44c875d6b2e",
+    "url": "/ColorPro/static/media/RSS_icon.ab350240.svg"
+  },
+  {
+    "revision": "c704551a9a14e60f97b874f426030207",
+    "url": "/ColorPro/static/media/TW_icon.c704551a.svg"
+  },
+  {
+    "revision": "c72e59a6d41ab917aa1e26928afc3633",
+    "url": "/ColorPro/static/media/baner_drukarnia.c72e59a6.png"
+  },
+  {
+    "revision": "dcf9dd798d807fbbf52cf0f05fda363d",
+    "url": "/ColorPro/static/media/baner_main.dcf9dd79.png"
+  },
+  {
+    "revision": "2a734c679b2ace6030fe51fba55f22bd",
+    "url": "/ColorPro/static/media/baner_main_sm.2a734c67.png"
+  },
+  {
+    "revision": "7d1148641f856e39ee327331eacbc96d",
+    "url": "/ColorPro/static/media/baner_studio_grafichne.7d114864.png"
+  },
+  {
+    "revision": "00aadb0869388dba44688fc423f091a3",
+    "url": "/ColorPro/static/media/baner_tworzenie_SI.00aadb08.png"
+  },
+  {
+    "revision": "33ee0279de7ab5c0670069454b9095f5",
+    "url": "/ColorPro/static/media/logo.33ee0279.svg"
+  },
+  {
+    "revision": "36f6883369692124983fb4e0f232e06c",
+    "url": "/ColorPro/static/media/pic_D_1.36f68833.png"
+  },
+  {
+    "revision": "7dcec649713d20b8bd7c5c5f4046e48a",
+    "url": "/ColorPro/static/media/pic_D_2.7dcec649.png"
+  },
+  {
+    "revision": "4fe797729fefe4b62c83b1031097f84e",
+    "url": "/ColorPro/static/media/pic_D_3.4fe79772.png"
+  },
+  {
+    "revision": "54c07081c44879d2d7d6c515a911ca94",
+    "url": "/ColorPro/static/media/pic_SG_1.54c07081.png"
+  },
+  {
+    "revision": "03d098476b886b7fa8dc2a39fda46e4c",
+    "url": "/ColorPro/static/media/pic_SG_2.03d09847.png"
+  },
+  {
+    "revision": "e080422be7d9dd2aecf6d1ed6a937a4b",
+    "url": "/ColorPro/static/media/pic_SG_3.e080422b.png"
+  },
+  {
+    "revision": "37eecb2b9f0291f4832448150515741a",
+    "url": "/ColorPro/static/media/pic_TSI_1.37eecb2b.png"
+  },
+  {
+    "revision": "c272ca211b4574b6a603f3ebd0a50448",
+    "url": "/ColorPro/static/media/pic_TSI_2.c272ca21.png"
+  },
+  {
+    "revision": "2f28c31bebad3ac835bb59a0dedf2397",
+    "url": "/ColorPro/static/media/pic_TSI_3.2f28c31b.png"
+  }
+]);
